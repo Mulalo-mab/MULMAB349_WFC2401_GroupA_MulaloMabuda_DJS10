525aee923fdf2978dc76727ce548a951bd17de37
Mulalo <mulalomabuda@gmail.com>
2024-06-13 21:51:40 +0200

    Update blogPosts.jsx
    removed an error

--- src/component/blogPosts.jsx
+++ src/component/blogPosts.jsx
@@ -1,6 +1,6 @@
 // src/components/blogPosts.js
 const blogPosts = async () => {
-  const response = await fetch("https://jsonplaceholder..typicode.com/posts");
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
